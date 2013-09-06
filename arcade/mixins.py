570cbbbb66de7c853d6d38e8f5ab0e1cfa856688
Matt Easterday <easterday@northwestern.edu>
2013-09-05 21:35:51 -0500

    added new mixin for lobby system

--- arcade/mixins.py
+++ arcade/mixins.py
@@ -5,6 +5,14 @@ For use with Namespaces -- that are generally useful for most simple projects, e
 
 
 """
+import logging
+import json
+import django
+
+from socketio.namespace import BaseNamespace
+
+from arcade.sdjango import namespace
+from arcade.models import Player, Room, Game
 
 
 class RoomsMixin(object):
@@ -82,4 +90,99 @@ class BroadcastMixin(object):
 			for sessid, socket in self.socket.server.sockets.iteritems():
 				if socket is not self.socket:
 					socket.send_packet(pkt)
+
+
+
+
+class LobbyMixin(BaseNamespace, RoomsMixin, BroadcastMixin):
+
+	DIR_NAME = ""
+
+	def initialize(self):
+		self.logger = logging.getLogger("socketio.playpolitics")
+		self.log("Socketio session started")
+    	
+	def log(self, message):
+		self.logger.info("[{0}] {1}".format(self.socket.sessid, message))
+    
+	def recv_disconnect(self):
+		# Remove nickname from the list.
+		self.log('Disconnected')
+		self.disconnect(silent=True)
+		return True
+
+	def on_enter_zone(self, user_id, nickname):
+		""" Called after connection when client wants to play state of nature."""		
+		
+		user = django.contrib.auth.get_user_model().objects.get(pk=user_id)
+		player, created = Player.objects.get_or_create(user=user, nickname=nickname)
+
+		# shouldn't return all the rooms for state of nature, only open ones...		
+		self.broadcast_event('rooms_changed')
+
+	def on_create_room(self, room_name):
+		""" 
+		User tries to create a new state of nature game
+		@param room_name as a string
+		"""
+		if (room_name == "")  or (room_name == None):
+			print "ERROR: room name cannot be null in on_create_room"
+			return
+
+		game = Game.objects.get(dir=self.DIR_NAME)
+		room, created = Room.objects.get_or_create(game=game, name=room_name)		
+		
+		self.broadcast_event('rooms_changed')
+
+	def on_join_room(self, room_id, user_id):		
+		room 	= Room.objects.get(pk=room_id)		
+		user 	= django.contrib.auth.get_user_model().objects.get(pk=user_id)
+		player 	= Player.objects.get(user=user)
+
+		if room.status == "getting-ready":
+			room.players.add(player)
+		else:
+			# can't join room if over or in-progress (should probably send error message)
+			# only exception is if you're already in game (because maybe you got kicked out by browser...)
+			if not (player in room.players.all()):
+				return
+		self.room = room_id
+		self.join(self.room)
+		self.emit('joined_room')
+		self.broadcast_event('rooms_changed') #broadcast to others...		
+
+	def on_send_room_list(self, user_id):
+		user 	= django.contrib.auth.get_user_model().objects.get(pk=user_id)
+		player 	= Player.objects.get(user=user)
+		self.emit('room_info', self._get_rooms(game_dir=self.DIR_NAME, player=player))  #broadcast to others...
+
+	def _get_rooms(self, game_dir, player):
+		rooms = []
+		game = Game.objects.get(dir=game_dir)
+		for room in sorted(game.rooms.all(), key=self._sort_rooms):
+			r = dict()
+			r['name'] 			= room.name
+			r['id'] 			= room.id		
+			#r['min_players']	= room.game.min_players
+			#r['max_players']	= room.game.max_players	
+			r['status']			= room.status
+			r['players']		= [aplayer.nickname for aplayer in room.players.all()]			
+
+			if room.status == 'getting-ready':
+				r['is_joinable'] = True
+			else:				
+				r['is_joinable'] = (player in room.players.all())
+
+			rooms.append(r)
+		return rooms
+
+	def _sort_rooms(self, room):
+		""" converts room status to a number for sorting. """
+		return {
+    		'getting-ready':0,
+    		'in-progress':1,
+    		'over':2}[room.status]
+
+
+
 
